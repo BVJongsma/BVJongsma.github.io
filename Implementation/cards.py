f0f2ec7ecab9c362fd81c101ba62e5a6a851b692
BVJongsma <b.v.jongsma@student.rug.nl>
2022-06-09 12:19:29 +0200

    pseudocode implementation added

---
+++ Implementation/cards.py
@@ -0,0 +1,22 @@
+__authors__="Bonne", "Naomï", "Iris"
+
+nweapons = 4
+npeople = 4
+nlocations = 0
+n = nweapons+npeople+nlocations
+nplayers=3
+
+class Card: #define the different cards (using hierachy of cards -> make three different kinds of cards)
+    def initiate_cards(self):
+        return
+
+class Weapon:
+    True
+
+class Location:
+    False
+
+class Color:
+    True
+
+
